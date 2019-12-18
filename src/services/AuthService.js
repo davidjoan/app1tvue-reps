304eaf3945d6134f42d7c99721c875e096faa9fc
David Tataje <davidtataje@gmail.com>
2019-12-17 19:23:34 -0500

    dotenv implemented

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -1,36 +1,38 @@
-import store from '../store/index'
+import store from "../store/index";
 import Axios from "axios";
 
-const API_URL = 'http://localhost:8000';
-
 class AuthService {
+  login(email, password) {
+    return Axios.post(`${process.env.VUE_APP_API_URL}/api/v1/login`, {
+      email: email,
+      password: password
+    })
+      .then(function(response) {
+        const token = response.data.data.success.token;
+        const message = response.data.message;
+        store.commit("authenticated", { token, message });
+        Axios.defaults.headers.common["Authorization"] = "Bearer " + token;
+        return response;
+      })
+      .catch(function(error) {
+        return error;
+      });
+  }
 
-    login(email, password) {
-        return Axios.post(`${API_URL}/api/v1/login`, {'email': email, 'password': password}).
-        then(function (response) {
-            const token   = response.data.data.success.token
-            const message = response.data.message
-            store.commit('authenticated', {token, message})
-            Axios.defaults.headers.common['Authorization'] = 'Bearer '+token
-            return response
-        }).catch(function (error) {
-            return error
-        });
-    }
+  logout() {
+    store.commit("logout");
+  }
 
-    logout(){
-        store.commit('logout')
-    }
-
-    getUser() {
-        return Axios.get(`${API_URL}/api/v1/getUser`).
-        then(function (response) {
-            store.commit('setUser', response.data.data)
-            return response
-        }).catch(function (error) {
-            return error
-        });
-    }
+  getUser() {
+    return Axios.get(`${process.env.VUE_APP_API_URL}/api/v1/getUser`)
+      .then(function(response) {
+        store.commit("setUser", response.data.data);
+        return response;
+      })
+      .catch(function(error) {
+        return error;
+      });
+  }
 }
 
-export default AuthService
+export default AuthService;
